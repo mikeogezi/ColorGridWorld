7de13ed9451f315c1ebf7c15b09b87448e956887
Michael Ogezi <okibeogezi@outlook.com>
2022-04-04 00:54:24 -0600

    feat: initial commit

---
+++ src/color_board_env.py
@@ -0,0 +1,191 @@
+from copy import deepcopy
+import gym
+from gym.envs.registration import register
+from gym import spaces
+import time
+import numpy as np
+import sys
+from termcolor import colored, cprint
+from collections import namedtuple
+import torch
+from transformers import BertTokenizer, BertModel
+
+registration = register(
+    id='ColorBoardEnv-v1',
+    entry_point='color_board_env:ColorBoardEnv',
+    max_episode_steps=None,
+)
+
+Position = namedtuple('Position', ['row', 'col'])
+
+REWARDS = {
+    'win': 5,
+    'invalid_stop': -5,
+    'move': -1,
+    'invalid_move': -1,
+}
+COLOR_MAP = {
+    'red': 1,
+    'green': 2,
+    'yellow': 3,
+    'blue': 4,
+    'magenta': 5,
+    'cyan': 6,
+    'white': 7,
+}
+COLORS = list(COLOR_MAP.keys())
+COLOR_CODES = list(COLOR_MAP.values())
+EMBEDDING_SIZE = 128
+BOARD_ROWS = 4
+BOARD_COLS = 4
+T = '◢◣'
+D = '◀▶'
+
+class ColorBoardEnv(gym.Env):
+    def __init__(self, seed, sleep_period_between_steps, text_encoder):
+        print('Starting up ColorBoard environment...')
+        self.start_time = time.time()
+        self.seed = seed
+        np.random.seed(self.seed)
+        self.sleep_period_between_steps = sleep_period_between_steps
+
+        self.tokenizer = BertTokenizer.from_pretrained(text_encoder)
+        self.model = BertModel.from_pretrained(text_encoder)
+
+        self.step_count = 0
+        self.reset_count = 0
+        
+        self.rewards = []
+        self.action_space = spaces.Discrete(5)
+        self.action_info_map = {
+            0: 'up',
+            1: 'right',
+            2: 'down',
+            3: 'left',
+            4: 'stop',
+        }
+        self.observation_space = spaces.Box(
+            low=0, high=len(COLOR_MAP), shape=(EMBEDDING_SIZE + (BOARD_ROWS * BOARD_COLS),))
+        self.reward_range = (-5, 5)
+
+        self.__board = None
+        self.__flattened_board = None
+        self.__player_position = None
+        self.__instruction = None
+        self.__target_color = None
+        self.__instruction_embedding = None
+
+    def reset(self):
+        self.rewards.clear()
+        self.step_count = 0
+        # self.seed += 1
+        self.reset_count += 1
+        self.__init_game()
+
+        return self.__observe()
+
+    def __init_game(self):
+        self.__board = np.random.randint(low=1, high=7, size=(BOARD_ROWS, BOARD_COLS))
+        self.__flattened_board = self.__board.reshape((1, BOARD_COLS * BOARD_ROWS))
+        self.__player_position = Position(np.random.randint(BOARD_ROWS), np.random.randint(BOARD_COLS))
+        self.__target_color = np.random.choice(COLORS)
+        self.__instruction = 'Go to a {} position'.format(self.__target_color)
+
+        inputs = self.tokenizer(self.__instruction, return_tensors="pt")
+        outputs = self.model(**inputs)
+        self.__instruction_embedding = outputs.last_hidden_state[:,:,:].mean(dim=1).detach().numpy()
+
+        print('🤖 starting position: ({}, {}); 🤖 target color: {}'.format(self.__player_position[0], self.__player_position[1], self.__target_color))
+
+    def step(self, action):
+        self.step_count += 1
+
+        time.sleep(self.sleep_period_between_steps)
+        action_str = self.action_info_map[action]
+        # print(action_str)
+
+        __current_row, __current_col = self.__player_position
+        __current_color = COLORS[self.__board[__current_row, __current_col] - 1]
+        __next = deepcopy(self.__player_position)
+        invalid_move = False
+        invalid_stop = False
+
+        done = False
+        if action_str == 'up':
+            if __current_row == 0:
+                invalid_move = True
+            else:
+                __next = Position(row=__current_row - 1, col=__current_col)
+        elif action_str == 'down':
+            if __current_row == (BOARD_ROWS - 1):
+                invalid_move = True
+            else:
+                __next = Position(row=__current_row + 1, col=__current_col)
+        elif action_str == 'right':
+            if __current_col == (BOARD_COLS - 1):
+                invalid_move = True
+            else:
+                __next = Position(row=__current_row, col=__current_col + 1)
+        elif action_str == 'left':
+            if __current_col == 0:
+                invalid_move = True
+            else:
+                __next = Position(row=__current_row, col=__current_col - 1)
+        elif action_str == 'stop':
+            if self.__target_color == __current_color:
+                done = True
+            else:
+                invalid_move = invalid_stop = True
+
+        self.__player_position = __next
+
+        if invalid_stop:
+            reward = REWARDS['invalid_stop']
+        elif invalid_move:
+            reward = REWARDS['invalid_move']
+        elif done:
+            reward = REWARDS['win']
+            cprint('🤖 wins!', 'white', 'on_green')
+        else:
+            reward = REWARDS['move']
+        
+        self.rewards.append(reward)
+        obs = self.__observe()
+        info = {}
+
+        return obs, reward, done, info
+
+    '''
+        (instruction_embeddings, player_position, board)
+    '''
+    def __observe(self):
+        return np.concatenate((self.__instruction_embedding, np.array(self.__player_position).reshape((1, 2)), self.__flattened_board), axis=1)
+
+    def render(self, mode):
+        print('/' + '-' * BOARD_COLS * 2 + '\\')
+        for r in range(BOARD_ROWS):
+            print('|', end='')
+            for c in range(BOARD_COLS):
+                pos_color_code = self.__board[r, c]
+                pos_color = COLORS[pos_color_code - 1]
+                on = 'on_{}'.format(pos_color)
+                if self.__is_player_position(Position(r, c)):
+                    cprint(T, 'grey', on, end='')
+                else:
+                    cprint('  ', '{}'.format(pos_color), on, end='')
+            print('|')
+        print('\\' + '-' * BOARD_COLS * 2 + '/')
+
+    def stop(self):
+        pass
+                
+    def __is_player_position(self, pos: Position):
+        return pos.row == self.__player_position.row and pos.col == self.__player_position.col
+
+    @property
+    def player_position(self):
+        return self.__player_position
+
+    @property
+    def instruction(self):
+        return self.__instruction
---
+++ src/main.py
@@ -0,0 +1,88 @@
+import time
+import gym
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+from color_board_env import ColorBoardEnv, BOARD_ROWS, BOARD_COLS, EMBEDDING_SIZE
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from keras.optimizer_v2.adam import Adam
+import keras.backend as K
+
+from rl.agents.dqn import DQNAgent
+from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+from rl.callbacks import FileLogger, ModelIntervalCheckpoint
+import argparse
+
+from stable_baselines3.common.monitor import Monitor
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', '-s', help='Seed to be used in generating random numbers', type=int, default=42)
+parser.add_argument('--sleep_period', '-S', help='Sleep period between steps made by the agent in seconds', type=float, default=0.)
+parser.add_argument('--text_encoder', '-e', help='Huggingface encoder to be used in encoding instructions', type=str, default='google/bert_uncased_L-2_H-128_A-2')
+parser.add_argument('--results_dir', '-r', type=str, default='./training_results')
+parser.add_argument('--weights_path', '-o', type=str, default='./weights.bin')
+parser.add_argument('--visualize', '-v', type=bool, default=True)
+args = parser.parse_args()
+
+if not os.path.exists(args.results_dir):
+  os.makedirs(args.results_dir)
+
+WINDOW_LENGTH = 4
+
+class Main():
+    def __init__(self, weights_path=args.weights_path):
+        self.weights_path = weights_path
+
+        self.max_steps_per_episode = 10000
+        self.total_steps = 100000
+        self.samp_freq = 10
+        self.max_episodes = 2
+        self.env = gym.make('ColorBoardEnv-v1', seed=args.seed, sleep_period_between_steps=args.sleep_period, text_encoder=args.text_encoder)
+        print('ColorBoardEnv-v1 env initialised')
+ 
+        self.env = Monitor(self.env, os.path.join(args.results_dir, '{}'.format(int(time.time()))), allow_early_resets=True)
+        print('Env wrapped in monitor')
+
+        self.num_actions = self.env.action_space.n
+        self.dqn_model = self.build_model()
+
+        self.load_model()
+        cbs = [ModelIntervalCheckpoint(self.weights_path, verbose=1, interval=1000)]
+        self.dqn_model.fit(self.env, nb_steps=self.total_steps, nb_max_episode_steps=self.max_steps_per_episode,
+            visualize=args.visualize, log_interval=100, callbacks=cbs)
+        self.env.stop()
+
+    def build_model(self):
+        model = Sequential()
+        input_shape = (WINDOW_LENGTH, 1, EMBEDDING_SIZE + 2 + (BOARD_ROWS * BOARD_COLS))
+        model.add(Flatten(input_shape=input_shape))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dense(self.num_actions, activation='linear'))
+        model.summary()
+
+        memory = SequentialMemory(limit=self.total_steps, window_length=WINDOW_LENGTH)
+        policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', 
+            value_max=.35, value_min=.075, value_test=.05, nb_steps=self.total_steps)
+        
+        dqn = DQNAgent(model=model, policy=policy, gamma=.995, memory=memory,
+            nb_actions=self.num_actions, train_interval=4, delta_clip=1.)
+        dqn.compile(Adam(learning_rate=2.5e-4), metrics=['mae'])
+        return dqn
+
+    def save_model(self):
+        print('Saving model...')
+        self.dqn_model.save_weights(self.weights_path, overwrite=True)
+
+    def load_model(self):
+        print('Loading model...')
+        if os.path.exists(self.weights_path + '.index'):
+            self.dqn_model.load_weights(self.weights_path)
+            return True
+        return False
+
+
+if __name__ == '__main__':
+    Main()
